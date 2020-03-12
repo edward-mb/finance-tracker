b80bc66f00b8c6592cb82ac9c90143157497d681
Jose Eduardo Mendez Barrios <joseeduardomendezb@gmail.com>
2020-03-11 17:59:31 -0700

    add config .standard.yml

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -13,7 +13,7 @@ class Stock < ApplicationRecord
       new(ticker: ticker_symbol,
           name: client.company(ticker_symbol).company_name,
           last_price: client.price(ticker_symbol))
-    rescue => exception
+    rescue
       nil
     end
   end
